b6dafdb0fbca5d6e975148b21320f85c180e30cf
soutruoshim <sout.rahim@gmail.com>
2023-05-14 21:03:41 +0700

    dagger hilt

---
+++ app/src/main/java/com/srhdp/to_docompose/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package com.srhdp.to_docompose.di
+
+import android.content.Context
+import androidx.room.Room
+import com.srhdp.to_docompose.data.TodoDatabase
+import com.srhdp.to_docompose.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        TodoDatabase::class.java,
+        DATABASE_NAME,
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: TodoDatabase) = database.toDoDao()
+}
